55b17f4e41b948075d6a0a56408bcf9be8a7572f
TANWIE12 <adeytanwie@gmail.com>
2023-06-01 01:40:43 +0100

    edited header section, removed image div and made it more clickable by removing z index

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Me from '../../assets/black.jpg'
+
 import Headersocilas from './Headersocilas'
 import Particless from '../Particles'
 
