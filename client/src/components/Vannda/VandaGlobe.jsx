8914319c26ba058d58ea12b848f0353862771281
TANWIE12 <adeytanwie@gmail.com>
2023-06-19 18:12:09 +0100

    fixed navabar alignment

--- client/src/components/Vannda/VandaGlobe.jsx
+++ client/src/components/Vannda/VandaGlobe.jsx
@@ -1,33 +1,43 @@
 import React, { useState, useEffect, useRef } from "react";
-import BIRDS from "vanta/dist/vanta.birds.min";
+import GLOBE from "vanta/dist/vanta.globe.min";
 import * as THREE from "three";
 
-export  const MyComponent = () => {
+export const MyComponent = () => {
   const [vantaEffect, setVantaEffect] = useState(0);
   const vantaRef = useRef(null);
 
   useEffect(() => {
+    const handleResize = () => {
+      
+    };
+
     if (!vantaEffect) {
       setVantaEffect(
-     BIRDS({
+        GLOBE({
           el: vantaRef.current,
           THREE: THREE,
           mouseControls: true,
           touchControls: true,
           gyroControls: false,
-          minHeight: 600.0,
-          minWidth: 600.0,
+          minHeight: window.innerWidth <= 600 ? 900.0 : 600.0,
+          minWidth: window.innerWidth <= 600 ? 150.0 : 600.0,
           scale: 1.0,
           scaleMobile: 1.0,
-         
         })
       );
     }
-   
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the effect and event listener on component unmount
+    return () => {
+      if (vantaEffect) {
+        vantaEffect.destroy();
+      }
+      window.removeEventListener("resize", handleResize);
+    };
   }, [vantaEffect]);
-  return (
-    <div ref={vantaRef}>
-      
-    </div>
-  );
+
+  return <div ref={vantaRef}></div>;
 };
